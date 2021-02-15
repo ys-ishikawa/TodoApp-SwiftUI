ebc08bdb679f7e31e8d6f7ea527cfe465259dbe4
yosuke-ishikawa <yosuke1028.ishikawa@gmail.com>
2021-02-15 19:44:34 +0900

    プロジェクトの作成

---
+++ TodoApp/TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Yosuke Ishikawa on 2021/02/15.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
